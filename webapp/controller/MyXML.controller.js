d7973fb4f16f9823130bb05cbdf032b9909c416e
akrocky <abul64604@gmail.com>
2022-03-06 21:48:10 +0100

    update

--- webapp/controller/MyXML.controller.js
+++ webapp/controller/MyXML.controller.js
@@ -5,11 +5,17 @@ sap.ui.define([
     Controlle.extend  ("chip.controller.MyXML",{
         
         onInit:function (params) {
-            
+           this.oView=this.getView() 
         },
         onBtnClick:function () {
             var oInp=this.getView().byId("idInp")
             alert(oInp.getValue())
+        },
+        onReload:function () {
+            this.oView.byId("idEmpId").setValue("100");
+            this.oView.byId("idEmpName").setValue("sujon");
+            this.oView.byId("idEmpSal").setValue("9500");
+            this.oView.byId("idCurr").setValue("USD")
         }
     })
     
