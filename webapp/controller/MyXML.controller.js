8723f375053acf2108fa9c9e677e913c151d3709
akrocky <abul64604@gmail.com>
2022-03-07 08:55:25 +0100

    update

--- webapp/controller/MyXML.controller.js
+++ webapp/controller/MyXML.controller.js
@@ -5,7 +5,37 @@ sap.ui.define([
     Controlle.extend  ("chip.controller.MyXML",{
         
         onInit:function (params) {
-           this.oView=this.getView() 
+           this.oView=this.getView()
+           //create model obeject 
+           var oModel= new sap.ui.model.json.JSONModel()
+           //load or set data to model
+           oModel.setData({
+            "empStr":{
+                 "empId":5000,
+                 "empName":"kocju",
+                 "salary":6500,
+                 "currency":"EUR"
+            } ,
+            "empTab":[
+                   {
+                    "empId":9000,
+                    "empName":"hiki",
+                    "salary":3500,
+                    "currency":"USD"
+                   },{
+                    "empId":9001,
+                    "empName":"tunku",
+                    "salary":4500,
+                    "currency":"USD"
+                   }
+            ]  
+           })
+           //make the model aware to the application
+           sap.ui.getCore().setModel(oModel);
+           //binding syntax
+           var Osalary=this.getView().byId("idEmpSal")
+           Osalary.bindValue('/empStr/salary')
+
         },
         onBtnClick:function () {
             var oInp=this.getView().byId("idInp")
